1b29ada9f2d84d5450bf9921b0c3e11f5672fc1e
Simon Cropp <simon.cropp@gmail.com>
2023-08-19 12:14:27 +1000

    .

---
+++ src/NUnitTests/ModuleInit.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using VerifyTests;
+
+public static class ModuleInit
+{
+    #region Enable
+
+    [ModuleInitializer]
+    public static void Init() =>
+        VerifyAvalonia.Initialize();
+
+    #endregion
+
+    [ModuleInitializer]
+    public static void InitOther() =>
+        VerifierSettings.InitializePlugins();
+}
---
+++ src/XUnitTests/ModuleInit.cs
@@ -0,0 +1,14 @@
+﻿public static class ModuleInit
+{
+    #region Enable
+
+    [ModuleInitializer]
+    public static void Init() =>
+        VerifyAvalonia.Initialize();
+
+    #endregion
+
+    [ModuleInitializer]
+    public static void InitOther() =>
+        VerifierSettings.InitializePlugins();
+}
