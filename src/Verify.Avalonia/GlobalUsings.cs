44dc7c39f57c70a2f62d22779b9ec9286c8aaf4f
Simon Cropp <simon.cropp@gmail.com>
2023-08-20 12:37:54 +1000

    Create GlobalUsings.cs

---
+++ src/Verify.Avalonia/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿// Global using directives
+
+global using Avalonia.Controls;
+global using Avalonia.Controls.Chrome;
+global using Avalonia.Controls.Embedding;
+global using Avalonia.Controls.Presenters;
+global using Avalonia.Controls.Primitives;
+global using Avalonia.Controls.Shapes;
