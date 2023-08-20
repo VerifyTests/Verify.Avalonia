71f667a179905fbf9e160189ffbfb49869f58cd0
Simon Cropp <simon.cropp@gmail.com>
2023-08-20 12:43:38 +1000

    Update GlobalUsings.cs

--- src/Verify.Avalonia/GlobalUsings.cs
+++ src/Verify.Avalonia/GlobalUsings.cs
@@ -1,9 +1,11 @@
 ﻿// Global using directives
 
+global using Avalonia;
 global using Avalonia.Controls;
 global using Avalonia.Controls.Chrome;
 global using Avalonia.Controls.Embedding;
 global using Avalonia.Controls.Notifications;
 global using Avalonia.Controls.Presenters;
 global using Avalonia.Controls.Primitives;
+global using Avalonia.Controls.Remote;
 global using Avalonia.Controls.Shapes;
