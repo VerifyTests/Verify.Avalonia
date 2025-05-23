e04b3dfeb2344316701928908e30b6e4c6445398
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-05-23 13:00:23 +0200

    test: Add test project for fluent avalonia

---
+++ src/FluentAvaloniaTests/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using Avalonia;
+global using Avalonia.Headless;
+global using Avalonia.Headless.NUnit;
+global using TestableApp;
---
+++ src/FluentAvaloniaTests/TestAppBuilder.cs
@@ -0,0 +1,22 @@
+using FluentAvalonia.Styling;
+
+[assembly: AvaloniaTestApplication(typeof(TestAppBuilder))]
+
+public static class TestAppBuilder
+{
+    public static AppBuilder BuildAvaloniaApp() =>
+        AppBuilder.Configure<App>()
+            .UseSkia()
+            .UseHeadless(new () { UseHeadlessDrawing = false })
+            .AfterSetup(builder =>
+            {
+                if (builder.Instance is null)
+                    throw new ("Instance is null");
+                var theme = new FluentAvaloniaTheme
+                {
+                    PreferSystemTheme = false,
+                    PreferUserAccentColor = false
+                };
+                builder.Instance.Styles.Add(theme);
+            });
+}
